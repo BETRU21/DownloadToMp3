057b74caae14f9de0a89733a14e65cabb6b59dcf
Benjamin <benjamin.trudel.2@ulaval.ca>
2021-10-21 13:34:16 -0400

    add folder structure and model

---
+++ Model/Download2Mp3.py
@@ -0,0 +1,43 @@
+import youtube_dl
+
+class Download2Mp3:
+    def __init__(self):
+        self.notDownloaded = []
+        self.setupDownloadParam()
+
+    # Public functions
+
+    def downloadMusicFile(self, url):
+        if type(url) is not str:
+            raise TypeError("url argument is not a string.")
+        try:
+            with youtube_dl.YoutubeDL(self.ydlParams) as ydl:
+                ydl.download([url])
+        except Exception as e:
+            e = str(e)
+            self.notDownloaded.append(url)
+            raise RuntimeError("Something went wrong.")
+
+    def musicsNotDownloaded(self):
+        return self.notDownloaded
+
+    # Non-Public functions
+
+    def callableHook(self, response):
+        if response["status"] == "downloading":
+            dlPercent = round((response["downloaded_bytes"]*100)/response["total_bytes"],1)
+            eta = response["eta"]
+            downloadPercent = f"{dlPercent}%"
+            estimatedTime = f"{eta}s"
+            return downloadPercent, estimatedTime
+
+    def setupDownloadParam(self):
+        self.ydlParams = {
+        "progress_hooks": [self.callableHook],
+        'outtmpl': 'downloadMusics/%(title)s.%(ext)s',
+        'noplaylist': True,
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',}],}
